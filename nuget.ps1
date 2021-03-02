2a9ffce593283b6502294eb961bd0ea959d1445a
Chris Hamons <chris.hamons@microsoft.com>
2021-03-02 12:54:05 -0600

    Nuke

--- nuget.ps1
+++
@@ -1,4 +0,0 @@
-npm install
-dotnet pack
-cp C:\Users\chhamo\Programming\autorest.csharp\artifacts\packages\Debug\Microsoft.Azure.AutoRest.CSharp.3.0.0-beta.20210302.1.nupkg C:\Users\chhamo\nuget\
-rm -r C:\Users\chhamo\.nuget\packages\microsoft.azure.autorest.csharp\
