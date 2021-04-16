3ed2961df1fa9754b0bdb862fb92155733d7ffc7
Chris Hamons <chris.hamons@microsoft.com>
2021-04-16 15:46:01 -0500

    Add test project

---
+++ test/AutoRest.TestServer.Tests/EnumTests.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Reflection;
+using ExtensionClientName;
+using ExtensionClientName.Models;
+using NUnit.Framework;
+
+namespace AutoRest.TestServer.Tests
+{
+    public class EnumTests
+    {
+    }
+}
